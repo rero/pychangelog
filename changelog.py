26a7d8c52d4fa36928c37787701f117b85111ddc
PascalRepond <pascal.repond@rero.ch>
2022-04-06 10:55:12 +0200

    feat: filter issues by ignored labels
    Co-Authored-by: Pascal Repond <pascal.repond@rero.ch>

--- changelog.py
+++ changelog.py
@@ -9,34 +9,51 @@ from datetime import timedelta
 @click.option('--token', help='Github Token.')
 def generate_change_logs(token):
     """."""
-    print(token)
+    
+    # Settings
     g = Github(token)
     repo = g.get_repo("rero/rero-ils")
     staging = repo.get_branch('staging')
     main = repo.get_branch('master')
+
     from_date = main.commit.commit.committer.date
     to_date = staging.commit.commit.committer.date
+    ignore_labels = ["stale", "duplicate", "wontfix", "translations", "task"]
     issues = repo.get_issues(state='closed', since=from_date, sort='updated')
     out_issues = []
     out_prs = []
     for issue in get_all(issues):
-        if issue.updated_at > to_date + timedelta(minutes=1):
-            click.secho(f'{issue.title} {issue.updated_at} {to_date} end before', fg='yellow')
+        if issue.closed_at > to_date + timedelta(minutes=1):
+            click.secho(f'{issue.title} {issue.closed_at} {to_date} end after', fg='yellow')
             continue
         if issue.pull_request:
-            out_prs.append(issue)
+            pr = repo.get_pull(issue.number)
+            if pr.merged:
+                out_prs.append(issue)
         else:
-            out_issues.append(issue)
+            ignore_issue = 0
+            for label in issue.labels:
+                if label.name in ignore_labels:
+                    ignore_issue = 1
+                    break
+                else:
+                    ignore_issue = 0
+            if ignore_issue == 0:
+                out_issues.append(issue)
 
-    print("# Issues")
+# TODO: filter pull requests that are linked to issues: either using GarphQL or by using closed_at
+
+# TODO: Structure from labels
+
+    print("# Issues \n")
     for issue in out_issues:
-        number = issue.html_url.split('/').pop()
-        print(f'* {issue.title} [\#{number}]({issue.html_url})')
+        # number = issue.html_url.split('/').pop()
+        print(f'* {issue.title} [\#{issue.number}]({issue.html_url})')
 
-    print("# Pull Requests")
+    print("# Pull Requests \n")
     for issue in out_prs:
         number = issue.html_url.split('/').pop()
-        print(f'* {issue.title} [\#{number}]({issue.html_url}) [{issue.user.login}]({issue.user.html_url})')
+        print(f'* {issue.title} [\#{issue.number}]({issue.html_url}) [{issue.user.login}]({issue.user.html_url})')
 
 
 def get_all(results):
