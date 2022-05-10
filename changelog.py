67085917eb2ddcb8469bf26b8a77ce43c336a7f3
PascalRepond <pascal.repond@rero.ch>
2022-05-10 12:39:07 +0200

    feat: use git tag to compare
    * Use a git tag to compare releases
    * Refactor and clean up code
    * Improve markdown export
    
    Co-Authored-by: Pascal Repond <pascal.repond@rero.ch>

--- changelog.py
+++ changelog.py
@@ -3,39 +3,43 @@
 import click
 import configparser
 from github import Github
-from datetime import timedelta
+from datetime import datetime, timedelta
 import re
 
-# Read config
+# Read and parse config
 config = configparser.ConfigParser()
 config.read('config.ini')
+
+conf = config['conf']
+ignore_labels = conf['ignore_labels'].split(",")
+repo_slug = f"{conf['user']}/{conf['repo']}"
 
 @click.command()
 @click.option('--token', help='Github Token.',)
 def generate_change_logs(token):
-    """."""
-    
+    """Main command to generate change logs."""
+
     # Settings
     g = Github(token)
-    repo_name = f"{config['conf']['user']}/{config['conf']['repo']}"
-    repo = g.get_repo(repo_name)
-    new_release = repo.get_branch(config['conf']['new_release_branch'])
-    latest_release = repo.get_branch(config['conf']['latest_release_branch'])
-    ignore_labels = config['conf']['ignore_labels'].split(",")
+    repo = g.get_repo(repo_slug)
+    merging_branch = repo.get_branch(conf['merging_branch'])
+    from_date = get_tag_date(conf['from_tag'], repo)
+    to_date = get_tag_date(conf['to_tag'], repo)
 
-    from_date = latest_release.commit.commit.committer.date
-    to_date = new_release.commit.commit.committer.date
-    
+    # Get all closed issues and PRs
     click.secho('Fetching all closed issues and PRs...')
     all_closed_issues = get_all(repo.get_issues(state='closed', sort='updated'))
 
+    # Get only issues closed in the selected timeframe
     issues = []
-
     with click.progressbar(all_closed_issues, label=f'Filtering issues closed between {from_date.strftime("%d-%m-%Y")} to {to_date.strftime("%d-%m-%Y")}...') as bar:
         for issue in bar:
             if issue.closed_at > from_date + timedelta(minutes=1) and issue.closed_at <= to_date + timedelta(minutes=1):
                 issues.append(issue)
 
+
+# Separate issues and PRs in two distinct lists. Filter issues by
+# ignored_label and PRs by base_branch
     out_issues = []
     out_prs = []
 
@@ -43,7 +47,7 @@ def generate_change_logs(token):
         for issue in bar:
             if issue.pull_request:
                 pr = repo.get_pull(issue.number)
-                if pr.merged and (pr.base.label == config['conf']['user'] + ':' + config['conf']['new_release_branch']):
+                if pr.merged and (pr.base.label == f"{conf['user']}:{merging_branch.name}"):
                     out_prs.append(issue)
             else:
                 ignore_issue = 0
@@ -56,7 +60,8 @@ def generate_change_logs(token):
                 if ignore_issue == 0:
                     out_issues.append(issue)
 
-# Filter pull requests that are linked to issues by comparing their closed_at date
+# Filter pull requests that are linked to issues by looking in the PR
+# body for an issue-closing keyword and the issue number
     final_prs = []
     for pr in out_prs:
         linked_pr = False
@@ -74,10 +79,11 @@ def generate_change_logs(token):
 
     final_issues = final_prs + out_issues
 
-    export_file(final_issues)
+    export_file(final_issues, conf['from_tag'], conf['to_tag'], repo_slug)
 
 
 def get_all(results):
+    '''Takes a Github API paginated response and iterates through the results.'''
     n = 0
     current_page = results.get_page(n)
     while current_page:
@@ -85,6 +91,15 @@ def get_all(results):
             yield r
         n += 1
         current_page = results.get_page(n)
+
+def get_tag_date(tag_name, repo):
+    '''Takes a tag name and returns its latest commit date if it exists in the provided repo'''
+
+    for tag in get_all(repo.get_tags()):
+        if tag.name == tag_name:
+            return tag.commit.commit.committer.date
+        else:
+            return datetime.now()
 
 def write_issue(issue):
     '''Takes an issue or PR and returns summary as string.'''
@@ -99,8 +114,9 @@ def get_labels(issue):
     for label in issue.labels:
         yield label.name
 
-def export_file(issues):
-    '''Categorizes a list of issues and outputs the structure into a markdown file'''
+def export_file(issues, from_tag, to_tag, repo_slug):
+    '''Categorizes a list of issues and outputs it into a structured 
+    markdown changelog'''
 
     features = []
     enhancements = []
@@ -127,7 +143,12 @@ def export_file(issues):
             other.append(issue)
 
     with open('PYCHANGELOG.md', 'w') as f:
-        f.write('# Changelog\n')
+        f.write(f'''# Changelog
+
+## [{to_tag}](https://github.com/{repo_slug}/tree/{to_tag}) ({datetime.date(datetime.now())})
+
+[Full Changelog](https://github.com/{repo_slug}/compare/{from_tag}...{to_tag})
+''')
         if features:
             f.write('\n**New features:**\n')
             for issue in features:
